d6bd8dcc48f7757ab39edda514a346bad125d389
azat <zacsun03@gmail.com>
2023-08-17 15:54:56 +0600

    Revert "Form"
    This reverts commit 916097bbfc755ef1c601494a003ceb05bfa93666.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 
-import { Link, Route, Routes } from 'react-router-dom';
 import './assets/css/App.css';
-import Auth from './pages/Auth/Auth';
-import Header from './components/Base/Header&Footer/Header/Header';
-import Footer from './components/Base/Header&Footer/Footer/Footer';
 
 function App() {
   return (
-    <div className='' >
-      <Header/>
-      <Routes>
-        <Route path="/" element={<div className='text-[40px] text-center'>Main <br /><Link to="/auth/accdata">Go to form</Link></div>} />
-        <Route path="/auth/accdata" element={<Auth />} />
-      </Routes>
-      <Auth />
-      <Footer/>
+    <div className="App">
+     
     </div>
   );
 }
