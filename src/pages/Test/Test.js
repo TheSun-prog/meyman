fe4eb2d672c407dc74e9ce15d94c81b19cb97b97
azat <zacsun03@gmail.com>
2023-09-01 11:31:44 +0600

    Revert "azat"
    This reverts commit 619516ef72c518ad338e8dcc7393da3472c75d4f.

--- src/pages/Test/Test.js
+++ src/pages/Test/Test.js
@@ -4,11 +4,10 @@ import fetchData from "../../axios/axios";
 import ProfileInfo from "../../components/objectRegisterComponents/ProfileInfo/ProfileInfo";
 import ObjectRegisterPage from "../ObjectRegister/ObjectRegisterPage";
 import FilterModal from "../../components/hotelComponents/FilterModal/FilterModal";
-import PostReview from "../../components/reviewComponents/PostReview/PostReview";
 
 const Test = () => {
     return (
-        <PostReview/>
+        <FilterModal/>
     )
 }
 
