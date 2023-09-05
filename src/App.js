6fd7811547ef037be3d0f285128e3b65593542f4
azat <zacsun03@gmail.com>
2023-09-05 21:03:21 +0600

    merge elaman

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // React modules
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 // Pages
 import Test from "./pages/Test/Test";
 import Mainpage from "./pages/Mainpage/Mainpage";
@@ -8,47 +8,50 @@ import HotelPage from "./pages/HotelPage/HotelPage";
 import Room from "./pages/Room/Room";
 import Booking from "./pages/Booking/Booking";
 import Layout from "./components/Base/Layout/Layout";
+import BusinessOwnerNotification from "./pages/BusinessOwnerNotification/BusinessOwnerNotification";
+import FillingRoomDetails from "./pages/FillingRoomDetails/FillingRoomDetails";
+import AuthPage from "./pages/AuthPage/AuthPage";
+import ConfirmCode from "./pages/ConfirmCodePage/ConfirmCode";
+import RegPage from "./pages/RegPage/RegPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import ScrollToTop from "./components/Base/ScrollToTop/ScrollToTop";
 
 function App() {
-  return (
-    <>
-         <ScrollToTop/>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Mainpage />} />
-          <Route path="/hotelcatalog" element={<HotelCatalog />} />
-          <Route path="/hotelcatalog/:hotelId" element={<HotelPage />} />
-          <Route path="/hotelcatalog/:hotelId/:roomId" element={<Room />} />
-          <Route
-            path="/hotelcatalog/:hotelId/:roomId/booking"
-            element={<Booking />}
-          />
-            <Route path={'/about'} element={<AboutPage/>}/>
-                <Route path={'/register'} element={<RegPage/>}/>
-                <Route path={'/confirmCode'} element={<ConfirmCode/>}/>
-                <Route path={'/auth'} element={<AuthPage/>}/>
-            <Route
-                path="/FillingRoomDetails"
-                element={<FillingRoomDetails/>}
-          />
-          <Route
-            path="/BusinessOwnerNotification"
-            element={<BusinessOwnerNotification/>}
-          />
+    return (<>
+            <ScrollToTop/>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route path="/" element={<Mainpage/>}/>
+                    <Route path="/hotelcatalog" element={<HotelCatalog/>}/>
+                    <Route path="/hotelcatalog/:hotelId" element={<HotelPage/>}/>
+                    <Route path="/hotelcatalog/:hotelId/:roomId" element={<Room/>}/>
+                    <Route
+                        path="/hotelcatalog/:hotelId/:roomId/booking"
+                        element={<Booking/>}
+                    />
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/register'} element={<RegPage/>}/>
+                    <Route path={'/confirmCode'} element={<ConfirmCode/>}/>
+                    <Route path={'/auth'} element={<AuthPage/>}/>
+                    <Route
+                        path="/FillingRoomDetails"
+                        element={<FillingRoomDetails/>}
+                    />
+                    <Route
+                        path="/BusinessOwnerNotification"
+                        element={<BusinessOwnerNotification/>}
+                    />
 
-          <Route path="/test" element={<Test />} />
-          <Route
-            path="/*"
-            element={
-              <h1 className="text-center text-red-700 text-[50px] mt-10">
-                Страница не найдена
-              </h1>
-            }
-          />
-        </Route>
-      </Routes>
-    </>
-  );
+                    <Route path="/test" element={<Test/>}/>
+                    <Route
+                        path="/*"
+                        element={<h1 className="text-center text-red-700 text-[50px] mt-10">
+                            Страница не найдена
+                        </h1>}
+                    />
+                </Route>
+            </Routes>
+        </>);
 }
 
 export default App;
