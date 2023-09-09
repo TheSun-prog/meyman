dbef557512aeb44e87b2235b8d7c9bfc3239117c
elamanz <z.elaman699@gmail.com>
2023-09-09 19:12:03 +0600

    страница бизнес владелец

---
+++ src/pages/BusinessMainPage/BusinessMainPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function BusinessMainPage(props) {
+    return (
+        <div className="mx-auto w-[1240px]">
+            <div>
+                <div>
+                    <h1 className='font-quicksand text-[28px] font-normal'>
+                        Приветствуем вас на нашей платформе!
+                    </h1>
+                    <p className='font-quicksand text-[22px] font-normal mb-[40px]'>
+                        Приветствуем вас на нашей платформе!
+                    </p>
+                    
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default BusinessMainPage;
